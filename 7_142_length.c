a0f920be346a94d49b32f455088563861bd96e4f
Khandaker Mahfuzur Rahman <khandakermahfuzur@gmail.com>
2026-04-09 17:14:41 +0200

    Add files via upload

---
+++ 7_142_length.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char ch;
+    int len = 0;
+
+    printf("Enter a message: ");
+    ch = getchar();
+    while (ch != '\n')
+    {
+        len++;
+        ch = getchar();
+    }   
+    printf("Your message was %d characters long.", len);
+    
+    return 0;
+    
+
+}
