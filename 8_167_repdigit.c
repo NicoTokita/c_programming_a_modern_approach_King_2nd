43e5fc84ff0a05723b85a301b6ff4a97925c258b
Khandaker Mahfuzur Rahman <khandakermahfuzur@gmail.com>
2026-04-16 16:36:22 +0200

    Add files via upload

---
+++ 8_167_repdigit.c
@@ -0,0 +1,38 @@
+/* Checks numbers for repeated digits*/
+
+#include <stdbool.h>
+#include <stdio.h>
+
+int main(void)
+{
+    bool digit_seen[10] = {false};
+    int digit;
+    long n;
+
+    printf("Enter a number: ");
+    scanf("%ld", &n);
+
+    while (n > 0)
+    {
+        digit = n % 10;
+
+        if (digit_seen[digit])
+        {
+            break;
+        }
+
+        digit_seen[digit] = true;
+
+        n /= 10;
+    }
+
+    if (n > 0)
+    {
+        printf("Repeated Digit\n");
+    }
+    else
+    {
+        printf("No Repeated Digit");
+    }
+    return 0;
+}
